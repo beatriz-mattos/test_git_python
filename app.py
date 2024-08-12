128a700e28225a0e8b536cd53be191fe19723681
beatriz-mattos <bmattos@outlook.de>
2024-08-12 09:10:44 -0300

    update app.py

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template
-import pandas as pd
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+from flask import Flask
+from routes import main
 
 
-df = pd.DataFrame({
-    'alunos': ['Renato', 'Fernando', 'Rodrigo', 'Ana', 'Joana', 'Silvio', 'Carolina'],
-    'notas': [15.00, 39.58, 62.92, 41.46, 48.33, 63.13, 70.00]
-})
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(main)
 
-@app.route('/table')
-def table():
-    # Ordena o dataframe pela coluna 'notas' em ordem decrescente
-    sorted_df = df.sort_values(by='notas', ascending=False)
+    return app
 
-    # Converte o dataframe ordenado para html e passa como parâmetro para o template table
-    return render_template('table.html', table=sorted_df.to_html(classes='table table-custom'))
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
