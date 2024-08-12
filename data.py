eb33a9ff47598ae7a6c0a9dfb00621366a7bc261
beatriz-mattos <bmattos@outlook.de>
2024-08-12 09:11:00 -0300

    create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    'alunos': ['Renato', 'Fernando', 'Rodrigo', 'Ana', 'Joana', 'Silvio', 'Carolina'],
+    'notas': [15.00, 39.58, 62.92, 41.46, 48.33, 63.13, 70.00]
+})
